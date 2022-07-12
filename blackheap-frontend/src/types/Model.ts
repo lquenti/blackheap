b9517e48f76c2d905151e8b05d8982bb3e409f7f
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-07-12 15:44:55 +0200

    :construction: add rust types to typescript

---
+++ blackheap-frontend/src/types/Model.ts
@@ -0,0 +1,52 @@
+enum BenchmarkType {
+  RandomUncached = "RandomUncached",
+  SameOffset = "SameOffset",
+};
+
+type Cluster = {
+  xs: Array<number>,
+  ys: Array<number>,
+  maximum: [number, number],
+}
+
+type BenchmarkKde = {
+  access_size: number,
+  xs: Array<number>,
+  ys: Array<number>,
+  significant_cluster: Array<Cluster>,
+  global_maximum: [number, number],
+};
+
+type Interval = {
+  lower: number | null,
+  upper: number | null,
+}
+
+type Constant = {
+  valid_interval: Interval,
+  const_value: number,
+}
+
+type Linear = {
+  valid_interval: Interval,
+  slope: number,
+  y_intercept: number,
+}
+
+type ConstantLinear = {
+  constant: Constant,
+  linear: Linear,
+}
+
+
+type Analysis = {
+  benchmark_type: BenchmarkType,
+  is_read_op: boolean,
+  kdes: Array<BenchmarkKde>,
+  model: {model: ConstantLinear | Linear},
+};
+
+export type {BenchmarkType, Cluster, BenchmarkKde, Interval, Constant, Linear, ConstantLinear, Analysis};
+
+const Model = Array<Analysis>;
+export default Model;
