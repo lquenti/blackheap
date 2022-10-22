56cd89f803100944bd1eaf53c94e28f584e456bf
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-10-22 14:41:33 +0200

    Add file upload for csv

--- blackheap-frontend/src/routes/Evaluation.tsx
+++ blackheap-frontend/src/routes/Evaluation.tsx
@@ -1,5 +1,35 @@
-// TODO: CREATE HERO COMPONENT WITH CHILDREN
+
+
+// TODO: CREATE ONE SINGLE COMPONENT OUT
+
+import { useEffect, useState } from "react";
+import { useFilePicker } from "use-file-picker";
+
+// OF BOTH UPLOADERS
 const Evaluation = () => {
-  return <div>lol</div>;
+  const [openFileSelector, { filesContent }] = useFilePicker({
+    accept: ".csv",
+    limitFilesConfig: {max: 1},
+  });
+  const [enableRedirect, setEnableRedirect] = useState(false);
+  
+  useEffect(() => {
+    if (filesContent.length !== 0) {
+      console.log(filesContent[0].content);
+    }
+  }, [filesContent]);
+
+  return (
+    <div className="hero min-h-screen bg-base-100">
+    <div className="hero-content text-center text-base-content">
+      <div className="max-w-md">
+        <p className="py-6 text-3xl">Upload your Records <a href="https://lquenti.github.io/blackheap/book/SingleNode.html">(Documentation)</a></p>
+        <button onClick={openFileSelector} className="btn btn-primary">
+          Select
+        </button>
+      </div>
+    </div>
+  </div>
+  );
 };
 export default Evaluation;
