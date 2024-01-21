8e3d1f6f809d97564f3ce36892c667331e64c9ba
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-01-21 17:52:00 +0100

    add C ci

--- blackheap-benchmarker/src/c_code/benchmarker.c
+++ blackheap-benchmarker/src/c_code/benchmarker.c
@@ -1,5 +1,10 @@
 #include "benchmarker.h"
 
+void should_fail_ci() {
+    int x; // Variable x is declared but not used.
+    int y = 10 / 0; // Division by zero, which is undefined behavior.
+}
+
 struct tuple tuple_add(const struct tuple *a, const struct tuple *b) {
   struct tuple result = { a->a + b->a, a->b + b->b };
   return result;
