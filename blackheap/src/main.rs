3aece393069f9a57b1cc2cdc144aed2a81964dfa
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-02-22 19:19:16 +0100

    set up logging

--- blackheap/src/main.rs
+++ blackheap/src/main.rs
@@ -1,5 +1,7 @@
 use crate::cli::Cli;
 
+use tracing::info;
+
 use clap::Parser;
 
 mod cli;
@@ -7,8 +9,10 @@ mod cli;
 
 fn main() {
     human_panic::setup_panic!();
-    let cli = Cli::parse();
+    tracing_subscriber::fmt::init();
 
+    info!("Parsing and validating CLI");
+    let cli = Cli::parse();
     if let Err(e) = cli::validate_cli(&cli) {
         println!("{:?}", e);
     }
