6fec888f77116a955b9250f512fedba57c201ef6
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-03-01 15:24:43 +0100

    Emmy specify SSD and which type of node

--- SLURM/Emmy.sh
+++ SLURM/Emmy.sh
@@ -2,6 +2,9 @@
 #SBATCH -t 1:00:00
 #SBATCH -p standard96
 #SBATCH --output=emmy-%j.out
+#SBATCH --exclusive
+#SBATCH --partition=standard96
+#SBATCH --constraint=ssd
 
 CLUSTER="emmy"
 
@@ -12,3 +15,5 @@ PATH_TO_SAVE_OUTPUT="${SCRIPT_DIR}/output_${CLUSTER}"
 PATH_TO_BENCHMARK_FILE="${SCRIPT_DIR}/benchmark_${CLUSTER}.dat"
 
 ./target/release/blackheap ${PATH_TO_SAVE_OUTPUT} -f ${PATH_TO_BENCHMARK_FILE}
+
+rm $PATH_TO_BENCHMARK_FILE
