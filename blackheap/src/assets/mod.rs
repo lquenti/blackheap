2aa9789de35994615784c4a26495ba67f0eb03ed
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-03-01 12:44:09 +0100

    add all assets to the binary

--- blackheap/src/assets/mod.rs
+++ blackheap/src/assets/mod.rs
@@ -7,14 +7,20 @@ use lazy_static::lazy_static;
 
 pub mod progress;
 
+const JUPYTER_NOTEBOOK: &[u8; 7528] = include_bytes!("../../assets/AnalysisTool.ipynb");
+const BUILD_MODELS: &[u8; 16151] = include_bytes!("../../assets/build_models.py");
+const GITIGNORE: &[u8; 3079] = include_bytes!("../../assets/.gitignore");
 const README: &[u8; 27] = include_bytes!("../../assets/README.md");
-const JUPYTER_NOTEBOOK: &[u8; 12] = include_bytes!("../../assets/AnalysisTool.ipynb");
+const REQUIREMENTS: &[u8; 47] = include_bytes!("../../assets/requirements.txt");
 
 lazy_static! {
     static ref FILES: HashMap<String, &'static [u8]> = {
         let mut map = HashMap::new();
-        map.insert(String::from("README.md"), &README[..]);
         map.insert(String::from("AnalysisTool.ipynb"), &JUPYTER_NOTEBOOK[..]);
+        map.insert(String::from("build_models.py"), &BUILD_MODELS[..]);
+        map.insert(String::from(".gitignore"), &GITIGNORE[..]);
+        map.insert(String::from("README.md"), &README[..]);
+        map.insert(String::from("requirements.txt"), &REQUIREMENTS[..]);
         map
     };
 }
