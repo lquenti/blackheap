ed21f87245a1281f19a761db95b3ec087b665d2c
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-03-02 18:49:38 +0100

    add slurm scripts emmy on local and scratch

--- SLURM/Emmy_lokal.sh
+++ SLURM/Emmy_lokal.sh
@@ -12,7 +12,7 @@ SCRIPT_DIR="/scratch-emmy/usr/$(whoami)/blackheap"
 cd $SCRIPT_DIR
 
 PATH_TO_SAVE_OUTPUT="${SCRIPT_DIR}/output_${CLUSTER}"
-PATH_TO_BENCHMARK_FILE="${SCRIPT_DIR}/benchmark_${CLUSTER}.dat"
+PATH_TO_BENCHMARK_FILE="/tmp/benchmark_${CLUSTER}.dat"
 
 ./target/release/blackheap ${PATH_TO_SAVE_OUTPUT} -f ${PATH_TO_BENCHMARK_FILE}
 
---
+++ SLURM/Emmy_scratch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -t 1:00:00
+#SBATCH -p standard96
+#SBATCH --output=emmy-%j.out
+#SBATCH --exclusive
+#SBATCH --partition=standard96
+#SBATCH --constraint=ssd
+
+CLUSTER="emmy"
+
+SCRIPT_DIR="/scratch-emmy/usr/$(whoami)/blackheap"
+cd $SCRIPT_DIR
+
+PATH_TO_SAVE_OUTPUT="${SCRIPT_DIR}/output_${CLUSTER}_scratch"
+PATH_TO_BENCHMARK_FILE="${SCRIPT_DIR}/benchmark_${CLUSTER}.dat"
+
+./target/release/blackheap ${PATH_TO_SAVE_OUTPUT} -f ${PATH_TO_BENCHMARK_FILE}
+
+rm $PATH_TO_BENCHMARK_FILE
