cf1ae256636bdacb267dc572d0ac6b30ea2fdcc6
Lars Quentin <lars@lquenti.de>
2025-05-07 13:09:25 +0200

    migrated to codeberg

---
+++ README.md
@@ -0,0 +1,3 @@
+# [See here for Blackheap](https://codeberg.org/LGLQ/LGBH)
+
+Blackheap now gets developed [as LGBH on Codeberg](https://codeberg.org/LGLQ/LGBH)
---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+// This is just here for the language classification tag, as the
+// codeberg codebase is written in C
+int main() {
+  printf("Please visit <https://codeberg.org/LGLQ/LGBH> for Blackheap");
+}
